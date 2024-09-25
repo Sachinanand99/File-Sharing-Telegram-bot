7a64022be39cf48f178fb3423fb8d32ace6f0089
Sachinanand99 <sachinanand10@hotmail.com>
2024-09-25 20:55:46 +0530

    removed user reply text

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, USER_REPLY_TEXT
 from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats','auth_secret','deauth_secret', 'auth', 'sbatch', 'exit', 'add_admin', 'del_admin', 'admins', 'add_prem', 'ping', 'restart', 'ch2l', 'cancel']))
--- plugins/useless.py
+++ plugins/useless.py
@@ -13,10 +13,10 @@ async def stats(bot: Bot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
-@Bot.on_message(filters.private & filters.incoming)
-async def useless(_, message: Message):
-    if message.from_user.id in ADMINS:
-        print(f"Admin {message.from_user.id} tried to access the useless function.")
-    if USER_REPLY_TEXT:
-        await message.reply(USER_REPLY_TEXT)
+# @Bot.on_message(filters.private & filters.incoming)
+# async def useless(_, message: Message):
+#     if message.from_user.id in ADMINS:
+#         print(f"Admin {message.from_user.id} tried to access the useless function.")
+#     if USER_REPLY_TEXT:
+#         await message.reply(USER_REPLY_TEXT)
 
